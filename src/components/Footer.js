5b4f574b2f42e1b73656a99f9db1fcc3f77e8105
skibina1 <askiba1705@gmail.com>
2019-10-09 23:15:13 +0200

    Add the URL to repo on github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../css/Footer.css';
-import logo from '../fb.png';
+import logo from '../icons/fb.png';
+import gitHub from '../icons/github.png';
 
 const Footer = () => (
     <footer className="footer">
       <p>Speedway Turbo Sliders & <a className="link" href="http://www.sts-pl.eu" >www.sts-pl.eu</a></p>
-      <p><a href="https://www.facebook.com/stspl.eu/"><img className="logoSize" src={logo} alt="Facebook"/></a></p>
+      <a href="https://www.facebook.com/stspl.eu/"><img className="logoSize" src={logo} alt="Facebook"/></a>
+      <a href="https://github.com/skibina1/podaj_trzy"><img className="logoSize" src={gitHub} alt="GitHub"></img></a>
     </footer>
   );
 
