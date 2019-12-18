9caaa1fc0f80cc5e34a1dc5c7c1392fe48a2544b
skibina1 <askiba1705@gmail.com>
2019-12-18 23:34:35 +0100

    Add ESlint to project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 import '../css/Footer.css';
 import logo from '../icons/fb.png';
 import gitHub from '../icons/github.png';
 
 const Footer = () => (
-    <footer className="footer">
-      <p>Speedway Turbo Sliders & <a className="link" href="http://www.sts-pl.eu" >www.sts-pl.eu</a></p>
-      <a href="https://www.facebook.com/stspl.eu/"><img className="logoSize" src={logo} alt="Facebook"/></a>
-      <a href="https://github.com/skibina1/podaj_trzy"><img className="logoSize" src={gitHub} alt="GitHub"></img></a>
-    </footer>
-  );
+  <footer className="footer">
+    <p>Speedway Turbo Sliders & <a className="link" href="http://www.sts-pl.eu" >www.sts-pl.eu</a></p>
+    <a href="https://www.facebook.com/stspl.eu/"><img className="logoSize" src={logo} alt="Facebook"/></a>
+    <a href="https://github.com/skibina1/podaj_trzy"><img className="logoSize" src={gitHub} alt="GitHub"></img></a>
+  </footer>
+);
 
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,51 +5,51 @@ import '../css/Main.css';
 var tracksList = tracks.tracks;
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            threeTracksList: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      threeTracksList: []
     }
+  }
 
-    pickThree() {
-        //console.log(tracksList);
-        const localThreeTracksList = [], usedNumber = [];
-        let iter = 0;
-        while (iter < 3) {
-            const index = Math.floor(Math.random() * tracksList.length);
-            if (usedNumber.includes(index)) {
-                continue;
-            } else {
-                usedNumber.push(index);
-            }
-            localThreeTracksList.push(tracksList[index]);
-            iter++;
-        }
-        //  console.log(localThreeTracksList);
-        this.setState({
-            threeTracksList: localThreeTracksList.slice()
-        });
+  pickThree() {
+    //console.log(tracksList);
+    const localThreeTracksList = [], usedNumber = [];
+    let iter = 0;
+    while (iter < 3) {
+      const index = Math.floor(Math.random() * tracksList.length);
+      if (usedNumber.includes(index)) {
+        continue;
+      } else {
+        usedNumber.push(index);
+      }
+      localThreeTracksList.push(tracksList[index]);
+      iter++;
     }
+    //  console.log(localThreeTracksList);
+    this.setState({
+      threeTracksList: localThreeTracksList.slice()
+    });
+  }
 
-    render() {
-        const { threeTracksList } = this.state;
+  render() {
+    const { threeTracksList } = this.state;
 
-        return (
-            <div className="Main">
-                <button type="button" className="btn btn-primary btn-lg" onClick={() => this.pickThree()} >Losuj tory</button>
-                <div className="Main-margin">
-                    {threeTracksList.length ? <div> {
-                        threeTracksList.map((item) => (
-                            <div className="Main-tracks">{item}</div>
-                        ))} </div>
-                        :
-                        <div className="Main-tracks">Tutaj pojawią sie trzy wylosowane tory</div>
-                    }
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="Main">
+        <button type="button" className="btn btn-primary btn-lg" onClick={() => this.pickThree()} >Losuj tory</button>
+        <div className="Main-margin">
+          {threeTracksList.length ? <div> {
+            threeTracksList.map((item, i) => (
+              <div key={i} className="Main-tracks">{item}</div>
+            ))} </div>
+            :
+            <div className="Main-tracks">Tutaj pojawią sie trzy wylosowane tory</div>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
